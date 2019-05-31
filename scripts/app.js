44af1e5bd95f079dfada9076290bae9fac6c437f
Lily Keeling <lily.keeling@gmail.com>
2019-05-31 17:23:33 +0100

    working rotation, moves left to right on keypress and down automatically. Stops at bottom and is fixed in place.

--- scripts/app.js
+++ scripts/app.js
@@ -3,26 +3,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
   const width = 10
   const squares = []
   let playerIndex = 25
-  let playerPos = []
+  let playerPos = [0,0,0,0]
+  let newPos = [0,0,0,0]
+
 
   function movePlayer() {
+    // console.log(playerPos)
     clear()
     // squares.forEach(square => square.classList.remove('player'))
-    squares[playerIndex].classList.add('player')
-    console.log(`the player should now move to position ${playerIndex}`)
+    // console.log(newPos)
+    // squares[playerIndex].classList.add('player')
+    squares[playerPos[0]].classList.add('player')
+    squares[playerPos[1]].classList.add('player')
+    squares[playerPos[2]].classList.add('player')
+    squares[playerPos[3]].classList.add('player')
+    // console.log(`the player should now move to position ${playerIndex}`)
 
   }
-
-  // const teeFunc = () => {
-  //   // if (document.querySelector('.player')) {
-  //   //   console.log('active')
-  //
-  //   squares[playerIndex + 10].classList.add('player')
-  //   squares[playerIndex + 11].classList.add('player')
-  //   squares[playerIndex + 9].classList.add('player')
-  //
-  // }
-
   const clear = () => {
     squares.forEach(square =>
       square.classList.remove('player')
@@ -35,6 +32,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     squares[playerIndex - 1].classList.add('player')
     squares[playerIndex + 9].classList.add('player')
     playerPos = [playerIndex, playerIndex - 11, playerIndex - 1, playerIndex + 9 ]
+    return playerPos
   }
 
   const teeLeft = () => {
@@ -43,6 +41,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     squares[playerIndex + 1].classList.add('player')
     squares[playerIndex + 11].classList.add('player')
     playerPos = [playerIndex, playerIndex - 9, playerIndex + 1, playerIndex + 11 ]
+    return playerPos
   }
 
   const teeDown = () => {
@@ -51,6 +50,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     squares[playerIndex  + 1].classList.add('player')
     squares[playerIndex + 10].classList.add('player')
     playerPos = [playerIndex, playerIndex - 1, playerIndex + 1, playerIndex + 10 ]
+    return playerPos
   }
 
   const teeUp = () => {
@@ -62,20 +62,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
     playerPos[1] = playerIndex - 1
     playerPos[2] = playerIndex + 1
     playerPos[3] = playerIndex -10
+    return playerPos
   }
 
   function rotatePress(e) {
 
     switch(e.keyCode) {
       case 49:
-      console.log(playerPos)
+      // console.log(playerPos)
+
       clear()
       teeRight()
 
       break
 
       case 50:
-      console.log(playerPos)
+      // console.log(playerPos)
       clear()
       teeDown()
 
@@ -83,7 +85,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
       case 51:
-      console.log(playerPos)
+      // console.log(playerPos)
       clear()
       teeLeft()
 
@@ -93,53 +95,61 @@ window.addEventListener('DOMContentLoaded', (event) => {
       case 52:
       clear()
       teeUp()
-      console.log(playerPos)
+      // console.log(playerPos)
       break
 
 
 
     }
+  }
+
+
+  const onBoardCheck = (playerPos) => {
+    return playerPos.every(pos =>
+      pos >= 0 && pos < 190
+
+    )
   }
 
 
 
   function leftRight(e) {
+    if (onBoardCheck(playerPos)) {
+      let playerShouldMove = true
 
-    let playerShouldMove = true
+      switch(e.keyCode) {
+        case 39:
+        if (playerIndex % width < width - 1){
+          // console.log(playerIndex)
+          // console.log(playerPos)
+          // playerPos.forEach(pos=>pos++)
+          // console.log(playerPos)
+          playerPos[0] ++
+          playerPos[1] ++
+          playerPos[2] ++
+          playerPos[3] ++
 
-    switch(e.keyCode) {
-      case 39:
-      if (playerIndex % width < width - 1){
+
+        }
+        break
+        case 37:
+        if (playerIndex % width > 0){
+
+          playerPos[0] --
+          playerPos[1] --
+          playerPos[2] --
+          playerPos[3] --
+        }
+        break
+
+        default: playerShouldMove = false
         // console.log(playerIndex)
-        playerIndex ++
       }
-      break
-      case 37:
-      if (playerIndex % width > 0){
-        // console.log(playerIndex)
-        playerIndex --
-      }
-      // break
-      // case 38:
-      // if (playerIndex - width >= 0){
-      //   console.log(playerIndex)
-      //
-      //   playerIndex -= width
-      // }
-      // break
-      // case 40:
-      // if (playerIndex + width < width * width * 2){
-      //   console.log(playerIndex)
-      //   playerIndex += width
-      // }
-      break
-      default: playerShouldMove = false
-      console.log(playerIndex)
+
+      if (playerShouldMove) movePlayer()
     }
-    if (playerShouldMove) movePlayer()
 
   }
-
 
 
 
@@ -148,11 +158,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   function init() {
 
-    //parent grid div
+
+
     const grid = document.querySelector('.grid')
 
 
-    console.log(grid)
 
 
     for (let i = 0; i < width * (width*2); i ++) {
@@ -163,11 +173,63 @@ window.addEventListener('DOMContentLoaded', (event) => {
       grid.append(square)
 
     }
-    squares[playerIndex].classList.add('player')
+
+    // const onBoardCheck = (playerPos) => {
+    //   return playerPos.every(pos =>
+    //     pos >= 0 && pos < 190
+    //
+    //   )
+    // }
+
+    const moveDown = () => {
+
+      // clear()
+      if (onBoardCheck(playerPos)) {
+        clear()
+        console.log('moving down')
+        playerPos[0] += width
+        playerPos[1] += width
+        playerPos[2] += width
+        playerPos[3] += width
+        squares[playerPos[0]].classList.add('player')
+        squares[playerPos[1]].classList.add('player')
+        squares[playerPos[2]].classList.add('player')
+        squares[playerPos[3]].classList.add('player')
+        // } else {
+        //   // console.log(onBoardCheck(playerPos))
+        //   // squares[playerPos[0]].classList.add('player fixed')
+        //   // squares[playerPos[1]].classList.add('player fixed')
+        //   // squares[playerPos[2]].classList.add('player fixed')
+        //   // squares[playerPos[3]].classList.add('player fixed')
+        //
+        // return
+
+
+
+      }
+
+
+
+
+
+    }
+
+    setInterval(moveDown, 1000)
+
+
+    // setInterval(moveDown, 100)
+
+    // squares[playerPos[0]].classList.add('player')
+    // squares[playerPos[1]].classList.add('player')
+    // squares[playerPos[2]].classList.add('player')
+    // squares[playerPos[3]].classList.add('player')
 
     window.addEventListener('keydown', leftRight)
     window.addEventListener('keydown', rotatePress)
   }
+
+
+
 
   init()
 
