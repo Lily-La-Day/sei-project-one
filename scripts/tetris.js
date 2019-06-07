bac53aad076229a3a7fed7ce34466ce473d85bc1
Lily Keeling <lily.keeling@gmail.com>
2019-06-07 05:04:28 +0100

    pushing after failed attempt to fix

--- scripts/tetris.js
+++ scripts/tetris.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const width = 10
   const squares = []
-  let playerIndex = 14
+  let playerIndex = 25
   let playerPos = [0,0,0,0]
   let nextPos = [0,0,0,0]
 
@@ -12,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
   let fixedSquaresTwo = [...document.querySelectorAll('[class*="fixed"]')]
   let score = document.querySelector('.score')
 
+init()
 
   const scoreFunction = () => {
     console.log(scoreTracker)
@@ -73,93 +74,85 @@ window.addEventListener('DOMContentLoaded', () => {
     )
   }
 
+//CORRECT STARTING POINTS TO BE ON GRID
 
   const TLZISJ0 = [
     {
-      Name: 'Tee',
-      start: [14, 3, 13, 23],
-      zero: [0, -11, -1, 9],
-      ninety: [0, -1, +1, 10],
-      oneEighty: [0, -9, +1, +11],
-      twoSeventy: [0, -1, +1, -10]
+      name: 'Tee',
+      ninety: [0, -11, -1, 9],
+      oneEighty: [0, -1, +1, 10],
+      twoSeventy: [0, -9, +1, +11],
+      threeSixty: [0, -1, +1, -10]
     },
 
     {
-      Name: 'Elle',     //NOT RIGHT
-      start: [14, 3, 13, 23],
-      zero: [0, -11, -1, 9],
-      ninety: [0, -1, +1, 10],
-      oneEighty: [0, -9, +1, +11],
-      twoSeventy: [0, -1, +1, -10]
-    },
-
-
-    {
-      Name: 'Zed',
-      start: [14, 4, 3, 15],
-      zero: [0, -10, -11, 1],
-      ninety: [0, 1, +10, -9],
-      oneEighty: [0, -1, +10, +11],
-      twoSeventy: [0, -10, -1, +9]
+      name: 'Elle',     //NOT RIGHT
+      ninety: [0, 10, 20, +21],
+      oneEighty: [0, -1, -2, +8],
+      twoSeventy: [0, -10, -20, -19],
+      threeSixty:  [0, 1, 2, -8],
     },
 
     {
-      Name: 'Eye',
-      start: [14, 15, 13, 12],
-      zero: [0, 1, -1, -2],
-      ninety: [0, -10, -20, +10],
-      oneEighty: [0, -1, 1, 2],
-      twoSeventy: [0, -10, +10, +20]
+      name: 'Zed',
+      ninety: [0, -1, 10 , -11],
+      oneEighty: [0, 10, -1, -9],
+      twoSeventy: [0, -10, -11, +1],
+      threeSixty: [0, -10, +1, -9]
     },
 
     {
-      Name: 'Ess',
-      start: [14, 4, 5, 13],
-      zero: [0, -10, -9, -1],
-      ninety: [0, -10, +1, +11],
-      oneEighty: [0, +1, +10, +9],
-      twoSeventy: [0, -10, +1, +11]
+      name: 'Eye',
+      ninety: [0, 10, -10, -20],
+      oneEighty: [0, -10, -20, +10],
+      twoSeventy: [0, +1, -1, -2],
+      threeSixty: [0, -10, +20, +10]
+    },
+
+    {
+      name: 'Ess',
+      ninety: [0, -10, -9, -1],
+      oneEighty: [0, -10, +1, +11],
+      twoSeventy: [0, +1, +10, +9],
+      threeSixty: [0, -10, +1, +11]
 
     },
 
     {
-      Name: 'Jay',
-      start: [14, 15, 13, 25],
-      zero: [0, +1, -1, +11],
-      ninety: [0, -10, -9, +10],
-      oneEighty: [0, -1, +1, -11],
-      twoSeventy: [0, +10, +9, -10]
+      name: 'Jay',
+      ninety: [0, +1, -10, -9],
+      oneEighty: [0, -10, -9, +10],
+      twoSeventy: [0, -1, +1, -11],
+      threeSixty: [0, +10, +9, -10]
 
     },
 
     {
       name: 'Oh',
-      start: [14, 13, 4, 3],
-      zero: [0, -1, -10, -11],
       ninety: [0, -1, -10, -11],
-      oneEighty: [ 0, -1, -10, -11],
-      twoSeventy: [0, -1, -10, -11]
+      oneEighty: [0, -1, -10, -11],
+      twoSeventy: [ 0, -1, -10, -11],
+      threeSixty: [0, -1, -10, -11]
 
     }
 
   ]
 
-  const makeShape = (playerIndex) => {
-
-    let rando = 0
-
+  const makeShape = () => {
+    console.log('making shape');
     const randomNum = Math.floor(Math.random()*7)
-    for (let i = 0; i < TLZISJ0[0].zero.length; i ++) {
-      rando = TLZISJ0[randomNum].zero[i]
-      squares[playerIndex + rando].classList.add('player')
-      playerPos.unshift(playerIndex+rando)
-      playerPos = playerPos.slice(0,4)
-    }
-    playerIndex = playerPos[3]
-    shapeName = TLZISJ0[randomNum].Name
-    console.log(shapeName)
-    return shapeName
+    for (let i = 0; i < 4; i++) {
+      playerPos[i] = playerIndex + TLZISJ0[randomNum].ninety[i]
 
+
+      for(let j = 0; j < squares.length; j ++) {
+
+        squares[playerPos[i]].classList.add('player')
+        player = TLZISJ0[randomNum].name
+      }
+
+    }
+    return player
   }
 
 
@@ -284,7 +277,7 @@ let rotateTracker = 0
 //Need to find a way to get shapename out of makeshape function
 //WORK THIS OUT FIRST THING IN THE MORNING!!!
 function rotate90() {
-
+console.log(playerIndex)
   if (shapeName === 'Tee'){
     playerPos[0] = (playerIndex + TLZISJ0[0].ninety[0])
     playerPos[1] = (playerIndex+ TLZISJ0[0].ninety[1])
@@ -344,6 +337,7 @@ function rotate90() {
 
 
 function rotate180() {
+  console.log(playerIndex)
   if (shapeName === 'Tee'){
     playerPos[0] = (playerIndex + TLZISJ0[0].oneEighty[0])
     playerPos[1] = (playerIndex+ TLZISJ0[0].oneEighty[1])
@@ -398,11 +392,12 @@ function rotate180() {
     // console.log(playerPos)
 
   }
-    playerIndex = playerPos.sort()[0]
+    playerIndex = playerPos[1]
   return rotateTracker = 180
 }
 
 function rotate270() {
+  console.log(playerIndex)
   if (shapeName === 'Tee'){
     playerPos[0] = (playerIndex + TLZISJ0[0].twoSeventy[0])
     playerPos[1] = (playerIndex+ TLZISJ0[0].twoSeventy[1])
@@ -456,66 +451,66 @@ function rotate270() {
     // console.log(playerPos)
 
   }
-    playerIndex = playerPos.sort()[0]
+    playerIndex = playerPos[1]
   return rotateTracker = 270
 }
 
 function rotate360() {
 
   if (shapeName === 'Tee'){
-    playerPos[0] = (playerIndex + TLZISJ0[0].zero[0])
-    playerPos[1] = (playerIndex+ TLZISJ0[0].zero[1])
-    playerPos[2] = (playerIndex + TLZISJ0[0].zero[2])
-    playerPos[3] = (playerIndex+ TLZISJ0[0].zero[3])
+    playerPos[0] = (playerIndex + TLZISJ0[0].threeSixty[0])
+    playerPos[1] = (playerIndex+ TLZISJ0[0].threeSixty[1])
+    playerPos[2] = (playerIndex + TLZISJ0[0].threeSixty[2])
+    playerPos[3] = (playerIndex+ TLZISJ0[0].threeSixty[3])
     // console.log(playerIndex)
     // console.log(playerPos)
   } else if (shapeName === 'Elle'){
-    playerPos[0] = (playerIndex + TLZISJ0[1].zero[0])
-    playerPos[1] = (playerIndex+ TLZISJ0[1].zero[1])
-    playerPos[2] = (playerIndex + TLZISJ0[1].zero[2])
-    playerPos[3] = (playerIndex+ TLZISJ0[1].zero[3])
+    playerPos[0] = (playerIndex + TLZISJ0[1].threeSixty[0])
+    playerPos[1] = (playerIndex+ TLZISJ0[1].threeSixty[1])
+    playerPos[2] = (playerIndex + TLZISJ0[1].threeSixty[2])
+    playerPos[3] = (playerIndex+ TLZISJ0[1].threeSixty[3])
     // console.log(playerIndex)
     // console.log(`new position should be ${playerPos}`)
   } else if (shapeName === 'Zed'){
-    playerPos[0] = (playerIndex + TLZISJ0[2].zero[0])
-    playerPos[1] = (playerIndex+ TLZISJ0[2].zero[1])
-    playerPos[2] = (playerIndex + TLZISJ0[2].zero[2])
-    playerPos[3] = (playerIndex+ TLZISJ0[2].zero[3])
+    playerPos[0] = (playerIndex + TLZISJ0[2].threeSixty[0])
+    playerPos[1] = (playerIndex+ TLZISJ0[2].threeSixty[1])
+    playerPos[2] = (playerIndex + TLZISJ0[2].threeSixty[2])
+    playerPos[3] = (playerIndex+ TLZISJ0[2].threeSixty[3])
     // console.log(playerIndex)
     // console.log(playerPos)
   } else if (shapeName === 'Eye'){
-    playerPos[0] = (playerIndex + TLZISJ0[3].zero[0])
-    playerPos[1] = (playerIndex+ TLZISJ0[3].zero[1])
-    playerPos[2] = (playerIndex + TLZISJ0[3].zero[2])
-    playerPos[3] = (playerIndex+ TLZISJ0[3].zero[3])
+    playerPos[0] = (playerIndex + TLZISJ0[3].threeSixty[0])
+    playerPos[1] = (playerIndex+ TLZISJ0[3].threeSixty[1])
+    playerPos[2] = (playerIndex + TLZISJ0[3].threeSixty[2])
+    playerPos[3] = (playerIndex+ TLZISJ0[3].threeSixty[3])
     // console.log(playerIndex)
     // console.log(playerPos)
   } else if (shapeName === 'Ess'){
-    playerPos[0] = (playerIndex + TLZISJ0[4].zero[0])
-    playerPos[1] = (playerIndex+ TLZISJ0[4].zero[1])
-    playerPos[2] = (playerIndex + TLZISJ0[4].zero[2])
-    playerPos[3] = (playerIndex+ TLZISJ0[4].zero[3])
+    playerPos[0] = (playerIndex + TLZISJ0[4].threeSixty[0])
+    playerPos[1] = (playerIndex+ TLZISJ0[4].threeSixty[1])
+    playerPos[2] = (playerIndex + TLZISJ0[4].threeSixty[2])
+    playerPos[3] = (playerIndex+ TLZISJ0[4].threeSixty[3])
     // console.log(playerIndex)
     // console.log(playerPos)
 
   } else if (shapeName === 'Jay'){
-    playerPos[0] = (playerIndex + TLZISJ0[5].zero[0])
-    playerPos[1] = (playerIndex+ TLZISJ0[5].zero[1])
-    playerPos[2] = (playerIndex + TLZISJ0[5].zero[2])
-    playerPos[3] = (playerIndex+ TLZISJ0[5].zero[3])
+    playerPos[0] = (playerIndex + TLZISJ0[5].threeSixty[0])
+    playerPos[1] = (playerIndex+ TLZISJ0[5].threeSixty[1])
+    playerPos[2] = (playerIndex + TLZISJ0[5].threeSixty[2])
+    playerPos[3] = (playerIndex+ TLZISJ0[5].threeSixty[3])
     // console.log(playerIndex)
     // console.log(playerPos)
 
   }  else if (shapeName === 'Oh'){
-    playerPos[0] = (playerIndex + TLZISJ0[6].zero[0])
-    playerPos[1] = (playerIndex+ TLZISJ0[6].zero[1])
-    playerPos[2] = (playerIndex + TLZISJ0[6].zero[2])
-    playerPos[3] = (playerIndex+ TLZISJ0[6].zero[3])
+    playerPos[0] = (playerIndex + TLZISJ0[6].threeSixty[0])
+    playerPos[1] = (playerIndex+ TLZISJ0[6].threeSixty[1])
+    playerPos[2] = (playerIndex + TLZISJ0[6].threeSixty[2])
+    playerPos[3] = (playerIndex+ TLZISJ0[6].threeSixty[3])
     // console.log(playerIndex)
     // console.log(playerPos)
 
   }
-    playerIndex = playerPos.sort()[0]
+    playerIndex = playerPos[1]
   return rotateTracker = 360
 }
 
@@ -556,10 +551,6 @@ playerPosAfter = (func) => {
 
 }
 
-
-
-
-
 function checkBelow(blockIndex){
 
   console.log(playerPos)
@@ -597,13 +588,7 @@ const spaceCheck = (playerPos) => {
 function left() {
 
   if (onBoardCheck(playerPos) && spaceCheck(playerPos)) {
-    // console.log(playerPos)
-
-    // let playerShouldMove = true
-    //
-    // switch(e.keyCode) {
-    //   case 39:
-      if ((playerIndex % width < width - 1)&&(boardLeftCheck(playerPos))){
+  if ((playerIndex % width < width - 1)&&(boardLeftCheck(playerPos))){
         playerPos[0] ++
         playerPos[1] ++
         playerPos[2] ++
@@ -613,7 +598,7 @@ function left() {
   }
 
   function right() {
-      if ((playerIndex % width > 0)&&(boardRightCheck(playerPos))){
+      if ((playerIndex % width > 0)&&(boardRightCheck(playerPos))&&nextPosRight()){
 
         playerPos[0] --
         playerPos[1] --
@@ -624,39 +609,22 @@ function left() {
 
     }
 
-
-
-
-
-
-
-
-function init() {
-  const grid = document.querySelector('.grid')
-  for (let i = 0; i < width * (width*2) + width + width; i ++) {
-    const square = document.createElement('div')
-    square.classList.add('grid-item')
-    square.innerHTML = i
-
-    square.dataset.index = i
-    squares.push(square)
-    grid.append(square)
-
-
-  }
-  for (let i = 210; i < 220 ; i ++) {
-    squares[i].dataset.row = 0
-    squares[i].style.opacity = 0
-  }
-  for (let i = 0; i < 10 ; i ++) {
-    squares[i].dataset.row = -1
-    squares[i].style.opacity = 0
-  }
-
-  makeShape(playerIndex)
+    makeShape(playerIndex)
 
   let newPos = [ squares[nextPos[0]], squares[nextPos[1]],
   squares[nextPos[2]], squares[nextPos[3]] ]
+
+  const nextPosLeft = () => {
+nextPos = playerPos.map(pos => pos  += 1)
+return nextPos
+
+  }
+
+  const nextPosRight = () => {
+nextPos = playerPos.map(pos => pos  -= 1)
+return nextPos
+
+  }
 
   const nextPosFunc = () => {
     nextPos = playerPos.map(pos => pos += width)
@@ -699,7 +667,7 @@ function init() {
       winFunction()
 
 
-      makeShape(14)
+      makeShape(15)
     }
 
   }
@@ -746,6 +714,29 @@ function init() {
     }
   }
 
+  function init() {
+    const grid = document.querySelector('.grid')
+    for (let i = 0; i < width * (width*2) + width + width; i ++) {
+      const square = document.createElement('div')
+      square.classList.add('grid-item')
+      square.innerHTML = i
+
+      square.dataset.index = i
+      squares.push(square)
+      grid.append(square)
+
+
+    }
+    for (let i = 210; i < 220 ; i ++) {
+      squares[i].dataset.row = 0
+      squares[i].style.opacity = 0
+    }
+    for (let i = 0; i < 10 ; i ++) {
+      squares[i].dataset.row = -1
+      squares[i].style.opacity = 0
+    }
+  }
+
 
   setInterval(moveDown, 500)
   setInterval(scoreFunction, 1000)
@@ -756,8 +747,8 @@ function init() {
 
   window.addEventListener('keydown', handleEventListeners)
 
-}
 
-init()
+
+
 
 })
