93b54fa2b69fe70a63e676d26f5b8a9e3796222a
Lily Keeling <lily.keeling@gmail.com>
2019-08-03 12:27:38 +0100

    make random shape function

--- scripts/take-two.js
+++ scripts/take-two.js
@@ -2,6 +2,7 @@ const height = 20
 const width = 10
 const active = 1 //true
 const direction = ''
+const startPoint = [5, 0]
 
 
 let tetronimos = new Array()
@@ -33,19 +34,39 @@ const init = () => {
 
 }
 
-const makeTetronimos = ()=> {
-  const eye = [[0,0], [0,1], [0,2], [0,3]]
+const makeTetronimos = () => {
+  const eye = [[0,0],[0,1], [0,2], [0,3]]
   const tee = [[0,0], [1,0], [2,0], [1,1]]
   const ess = [[2,0], [1,0], [1,1], [0,1]]
   const elle = [[2,0], [0, 1], [1, 1], [2,1]]
   const zed = [[0,0], [1,0], [1,1], [2,1]]
   const jay = [[2,0], [0, 1], [1, 1], [0,1]]
   const oh = [[0, 0], [0, 1], [1, 0], [1, 1]]
-  tetronimos = [...eye, tee, elle, jay, oh, jay, zed, ess]
+  tetronimos = [eye, tee, elle, jay, oh, jay, zed, ess]
   console.log(tetronimos)
 }
+
+const makeTetronimo = () => {
+  const random = Math.floor(Math.random() * tetronimos.length)
+  const tetronimo = tetronimos[random]
+
+  activated =
+  { shape: tetronimo,
+    location: startPoint
+  }
+
+  console.log(activated)
+
+
+
+
+
+}
+
+
 
 window.addEventListener('load', function(){
   init()
   makeTetronimos()
+  makeTetronimo()
 })
